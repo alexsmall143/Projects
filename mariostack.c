d37d242d8ac5325c4e03105f3de33bb9fadc1af8
alexsmall143 <104016742+alexsmall143@users.noreply.github.com>
2022-07-19 08:51:43 +0100

    Add files via upload

---
+++ mariostack.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height;
+    // getting user input
+    do
+    {
+        printf("Please enter desired tower height: ");
+        scanf("%i", &height);
+    }
+    //creating range for user
+    while (height > 25 || height < 1);
+    //creating new line based on hieght
+    for (int line = 0; line < height; line++)
+    {
+        //making pyramid left to right
+        for (int space = 0; space < height - line - 1; space++)
+        {
+            printf(" ");
+        }
+        //creating hash
+        for (int hash = 0; line >= hash; hash++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
